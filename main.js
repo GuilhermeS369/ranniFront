89fd48e2a7c5252d9e78690c51adb4c35fca7dfd
Guilherme Santos <guilherme460@yahoo.com.br>
2022-06-06 23:06:36 -0300

    Métodos de requisições básicas completas

--- main.js
+++ main.js
@@ -1,11 +1,66 @@
-const url = "https://ranni-app.herokuapp.com/users/1"
+const url = "https://ranni-app.herokuapp.com/users"
 
-function getUser(){
-    axios.get(url)
-    .then(response =>{
-        console.log(response)
-    })
-    .catch(error => console.log(error))
+
+const newUser = {
+    name: "Bob Brown",
+    email: "bob@gmail.com",
+    phone: "963495981",
+    password: "123456789"
 }
 
-getUser()
+const UserUpdated = {
+    name: "UsuarioEditado",
+    email: "editUser@gmail.com",
+    phone: "111111111",
+
+}
+
+function getUser() {
+    axios.get(url)
+        .then(response => {
+            const data = response.data
+            renderResults.textContent = JSON.stringify(data)
+        })
+        .catch(error => console.log(error))
+}
+//getUser()
+
+function addNewUser() {
+    axios.post(url, newUser)
+        .then(response => {
+            console.log(response.data)
+            alert("OK")
+        })
+        .catch(error => console.log(error))
+}
+//addNewUser()
+
+function updateUser() {
+    axios.put(`${url}/3`, UserUpdate)
+        .then(response => {
+            console.log(response.data)
+            alert("Update")
+        })
+        .catch(error => console.log(error))
+}
+//updateUser()
+
+function deleteUser() {
+    axios.delete(`${url}/20`)
+        .then(response => {
+            console.log(response.data)
+            alert("Delete")
+        })
+        .catch(error => console.log(error))
+}
+//deleteUser()
+
+function getOneUser() {
+    axios.get(`${url}/10`)
+        .then(response => {
+            const data = response.data
+            renderResults.textContent = JSON.stringify(data)
+        })
+        .catch(error => console.log(error))
+}
+//getOneUser()
